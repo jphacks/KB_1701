2899694e5494e40b0d6d68abca7ca1e387fab796
ytakaya <ytakaya@mikilab.doshisha.ac.jp>
2017-10-11 23:19:09 +0900

    エンドポイントとして/slackと/githubを追加

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/slack', function(req, res, next) {
+  console.log(req.body);
+  res.send('POST request to the /slack');
+});
+
+router.post('/github', function(req, res, next) {
+  console.log(req.body);
+  res.send('POST request to the /github');
+});
+
 module.exports = router;
