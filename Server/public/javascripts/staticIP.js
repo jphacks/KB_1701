2a09c3ba058dfce8c1c606defc5970d78ac707a3
tdomen <tdomen@mikilab.doshisha.ac.jp>
2017-10-22 04:44:41 +0900

    staticIPの仕様を変更

--- Server/public/javascripts/staticIP.js
+++ Server/public/javascripts/staticIP.js
@@ -2,12 +2,31 @@
 // <script src="/javascripts/staticIP.js">
 
 // 他のJSファイル内での使い方
-// const url = getHostUrl('/nameOfEndpoint/...');
+// const url = getHostUrl('https'(もしくは'ws'), '/nameOfEndpoint/...');
 // (特にimport等をする必要はなし)
 
-function getHostUlr(endpoint){
-    const ip = '192.168.128.102';
-    const hostUrl = 'https://' + ip + ':3000' + endpoint;
+function getHostUlr(type, endpoint){
+    const host = 'ec2-13-115-41-122.ap-northeast-1.compute.amazonaws.com';
+    const httpsPort = 3000;
+    const wsPort    = 8081;
+    let port;
 
+    // 型チェック
+    if (type === 'https'){
+	port = httpsPort;
+    }
+    else if(type === 'ws'){
+	port = wsPort;
+    }
+    else{
+	return alert('type: '+ type +' is invalid');
+    }
+
+    // endpointの形式を再成形
+    if (endpoint.slice(0, 1) !== '/'){
+	endpoint = '/' + endpoint;
+    }
+
+    const hostUrl = type + '://' + host + ':' + port + endpoint;
     return hostUrl;
 };
