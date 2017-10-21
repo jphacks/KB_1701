17777c88d4a77f5bbbaf59c7fa80ac3d988d8a42
tdomen <tdomen@mikilab.doshisha.ac.jp>
2017-10-22 03:38:26 +0900

    IPを固定しておく&ホストURLを取得する関数ファイル

--- Server/public/javascripts/staticIP.js
+++ Server/public/javascripts/staticIP.js
@@ -1,8 +1,13 @@
-const getHostUlr = function (){
+// ejsファイルのheaderの先頭に以下の記述を追加
+// <script src="/javascripts/staticIP.js">
+
+// 他のJSファイル内での使い方
+// const url = getHostUrl();
+// (特にimport等をする必要はなし)
+
+function getHostUlr(){
     const ip = '192.168.128.102';
     const hostUrl = 'https://' + ip + ':3000';
 
     return hostUrl;
 };
-
-export default getHostUlr;
