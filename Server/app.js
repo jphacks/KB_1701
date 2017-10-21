90d9960735dcaad29de45c7e95aef4a7cc94f1c6
tshimakawa <tsukasa.soccer.0922@gmail.com>
2017-10-22 00:01:37 +0900

    git merge

--- Server/app.js
+++ Server/app.js
@@ -1,78 +1,87 @@
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const http = require('http');
-const https = require('https');
-const fs = require('fs');
-const mongoose = require('mongoose');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-const app = express();
+// ---- Renamed and Added by tdomen -----
+var commits_regist = require('./routes/commits/commits_regist');
 
-const index = require('./routes/index');
-const users = require('./routes/users');
-const oauth = require('./routes/oauth');
-const lablive = require('./routes/lablive');
+var http = require('http');
+var mongoose = require('mongoose');
+// ----------------------------------------
 
-const ssloptions = {
-  key: fs.readFileSync('./serverKey/localhost.key', 'utf8'),
-  cert: fs.readFileSync('./serverKey/localhost.crt', 'utf8')
-};
-
-mongoose.Promise = global.Promise;
-const mongodbUri = 'mongodb://192.168.100.32/HackHack';
-const mongOptions = {
-    useMongoClient: true,
-    socketTimeoutMS: 0,
-    keepAlive: true,
-    reconnectTries: 30
-};
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// ポート設定
-app.set('httpsport', process.env.PORT || 3000);
-app.set('httpport', process.env.PORT || 4000);
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+// ---- Added by tdomen -----
+// ポート設定
+app.set('port', process.env.PORT || 20000);
+// --------------------------
+
 app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
-app.use('/oauth', oauth);
-app.use('/live', lablive);
+// ---- Added by tdomen -----
+// ルーティング機能
+app.use('/commits_regist', commits_regist);
+// --------------------------
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handlers
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  console.log("error1");
+    app.use(function(err, req, res, next) {
+      console.log("error2");
+      res.status(err.status || 500);
+      res.render('error', {
+    message: err.message,
+    error: err
+    });
+  });
+}
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  console.log("error3");
+    res.status(err.status || 500);
+    res.render('error', {
+    message: err.message,
+    error: {}
+    });
 });
 
 // サーバ立ち上げ
-var server = https.createServer(ssloptions,app).listen(app.get('httpsport'), function(){
-    console.log('Express HTTPS server listening on port ' + app.get('httpsport'));
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+    const mongodbUri = "mongodb://localhost/test";
+    const mongOptions = {
+      useMongoClient: true,
+      socketTimeoutMS: 0,
+      keepAlive: true,
+      reconnectTries: 30};
+    mongoose.Promise = global.Promise;
     mongoose.connect(mongodbUri, mongOptions);
 });
+// ------------------------------------------
 
 module.exports = app;
