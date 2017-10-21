793ae9a82de67d38d7e059ea829a14767548f5a3
Yamashita69 <dyamashita@mikilab.doshisha.ac.jp>
2017-10-21 19:19:57 +0900

    やましぃにレスポンシブはおもたいのです

--- Server/public/javascripts/timer.js
+++ Server/public/javascripts/timer.js
@@ -5,7 +5,7 @@ var cnt1 = "日";var cnt2 = "：";var cnt3 = "：";var cnt4 = ".";
 var baseoffset = "-9";var cuttime = "";
 var br1 = "";var br2 = "";var br3 = "";var br4 = "";
 var com1 = "";var com2 = "";var com3 = "";var com4 = "HackTime終了";var end = "2";
-var width = "400";var height = "100";
+var width = "400";var height = "50";
 var font = "arial";var font2 = "7seg4";var size = "27";var l_height = "27";var bold = "";var italic = "";var line = "";
 var space = "0px 0px 0px 0px";
 var align = "2";var img = "";
